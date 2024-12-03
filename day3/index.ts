c865b25f408ac4efdd93c29f358c40b16f0d7258
JuicyBenjamin <benjaminvinterberg@gmail.com>
2024-12-03 23:08:46 +0100

    feat(day3): part 2

--- day3/index.ts
+++ day3/index.ts
@@ -13,3 +13,31 @@ for (const match of matches) {
 
 // answer to part 1
 console.log(total)
+
+const dontArray = data.split("don't")
+const doArray: string[] = []
+
+dontArray.forEach((dontPart, index) => {
+  // The first element has an implied do
+  if (index === 0) doArray.push(dontPart)
+
+  dontPart.split('do').forEach((doPart, index) => {
+    // The first element has an implied dont
+    if (index === 0) return
+    doArray.push(doPart)
+  })
+})
+
+const doData = doArray.join('')
+
+const doMatches = doData.matchAll(multiplyRegex)
+
+let doTotal = 0
+
+for (const doMatch of doMatches) {
+  const [_, num1, num2 ] = doMatch
+  doTotal += Number(num1) * Number(num2)
+}
+
+// answer to part 2
+console.log(doTotal)
